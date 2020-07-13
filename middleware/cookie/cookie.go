ad86b43a587cd9f1f3c23b89dfb7b3fce9fce479
hoastar <hoastar>
2020-07-13 17:49:08 +0800

    add authcookie

---
+++ middleware/cookie/cookie.go
@@ -0,0 +1,24 @@
+package cookie
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func AuthCookie() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 获取客户端cookie并校验
+		if cookie, err := c.Cookie("testcookie"); err == nil {
+			if cookie == "q1w2e3r4" {
+				c.Next()
+				return
+			}
+		}
+
+		// 返回错误
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"error": "err"})
+		// 若验证不通过, 不再调用后续的函数处理
+		c.Abort()
+		return
+	}
+}
